d71c60d7664579e07caad4ac0c7938ddd2225cf3
WaqasAkhtar1 <mwaqasakhtar101@gmail.com>
2021-12-02 03:36:50 -0800

    my first commit

---
+++ src/First_GitHub_Project/First_File.java
@@ -0,0 +1,11 @@
+package First_GitHub_Project;
+
+public class First_File {
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String name = "Momin";
+		String rollno = "18i-0574";
+		System.out.println("Name: " + name);
+		System.out.println("RollNo: " + rollno);
+	}
+}
